6b82e0702f6a784a0af59d0620cb886309325d2a
Seamus Levin <sjlevin72@gmail.com>
2025-04-17 10:29:54 +0100

    Fix wouter import syntax for better compatibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import * as wouter from "wouter";
+const { Switch, Route } = wouter;
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 // import { Toaster } from "@/components/ui/toaster";
